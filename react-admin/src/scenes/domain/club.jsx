3c12b048d18848c4f10d8864cbbdb14989d43705
JungHyeonmin <2ee42a45@gmail.com>
2024-10-14 17:49:15 +0900

    Feat: 매치 조회 페이지

--- react-admin/src/scenes/domain/club.jsx
+++ react-admin/src/scenes/domain/club.jsx
@@ -3,24 +3,11 @@ import { DataGrid } from "@mui/x-data-grid"; // DataGrid는 MUI의 테이블을 
 import { tokens } from "../../theme"; // theme.js 파일에서 가져온 tokens로 테마 색상 사용
 import { useEffect, useState } from "react"; // React의 useEffect 및 useState 훅 임포트
 import axios from "axios"; // axios를 사용하여 API 요청 처리
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined"; // 관리자 아이콘
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined"; // 일반 유저 아이콘
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined"; // 매니저 아이콘
 import Header from "../../components/Header"; // 페이지의 제목과 부제목을 보여주는 Header 컴포넌트
 import { getClubList } from './apis/clubAPI';
 
-/*
-const getCookie = (name) => {
-  const value = `; ${document.cookie}`; // 현재 쿠키를 문자열로 가져옴
-  const parts = value.split(`; ${name}=`); // 이름으로 쿠키를 찾기 위해 분할
-  if (parts.length === 2) return parts.pop().split(';').shift(); // 쿠키 값 반환
-};
-*/
-
-
-
-// Team 컴포넌트: 팀 데이터를 테이블로 보여주는 컴포넌트
-const ClubList = () => {
+// CLub 컴포넌트: CLub 데이터를 테이블로 보여주는 컴포넌트
+const Club = () => {
 
   const theme = useTheme(); // MUI 테마 훅을 사용하여 현재 테마 정보를 가져옴
   const colors = tokens(theme.palette.mode); // 테마 모드 (다크/라이트)에 따라 colors 정의
@@ -32,7 +19,7 @@ const ClubList = () => {
   const [userInfo, setUserInfo] = useState({});
 
 
-  // 팀 데이터를 가져오는 useEffect 훅
+  // Club 데이터를 가져오는 useEffect 훅
   useEffect(() => {
     // 로그인 여부 확인 API 호출
     axios.get('http://localhost:8080/api/auth/status', { withCredentials: true })
@@ -104,45 +91,12 @@ const ClubList = () => {
       headerName: "createdAt",
       flex: 1,
     }
-    // {
-    //   field: "accessLevel",
-    //   headerName: "Access Level", // 권한 수준 필드
-    //   flex: 1, // 열의 크기 유연하게 조정
-    //   renderCell: ({ row: { access } }) => { // 각 셀을 커스텀 렌더링
-    //     return (
-    //       <Box
-    //         width="60%" // 셀 너비
-    //         m="0 auto" // 자동으로 가로 중앙 정렬
-    //         p="5px" // padding 값 설정
-    //         display="flex" // 플렉스 박스 레이아웃
-    //         justifyContent="center" // 아이템을 중앙에 배치
-    //         backgroundColor={
-    //           access === "admin"
-    //             ? colors.greenAccent[600] // 관리자는 greenAccent 색상
-    //             : access === "manager"
-    //               ? colors.greenAccent[700] // 매니저는 조금 더 어두운 greenAccent 색상
-    //               : colors.greenAccent[700] // 일반 유저도 동일한 색상
-    //         }
-    //         borderRadius="4px" // 모서리를 둥글게 만듦
-    //       >
-    //         {/* access 값에 따라 다른 아이콘을 표시 */}
-    //         {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-    //         {access === "manager" && <SecurityOutlinedIcon />}
-    //         {access === "user" && <LockOpenOutlinedIcon />}
-    //         {/* access 텍스트를 표시, 색상은 테마의 grey[100] 사용 */}
-    //         <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-    //           {access} {/* access 값 출력 */}
-    //         </Typography>
-    //       </Box>
-    //     );
-    //   },
-    // }
   ];
 
 
   // 로딩 중일 때의 처리
   if (loading) {
-    return <Typography>Loading...</Typography>; // 로딩 중일 경우 텍스트 표시
+    return <Typography>Club Loading...</Typography>; // 로딩 중일 경우 텍스트 표시
   }
 
 
@@ -192,4 +146,4 @@ const ClubList = () => {
   );
 };
 
-export default ClubList; // Team 컴포넌트를 기본으로 내보냄
+export default Club; // Team 컴포넌트를 기본으로 내보냄
