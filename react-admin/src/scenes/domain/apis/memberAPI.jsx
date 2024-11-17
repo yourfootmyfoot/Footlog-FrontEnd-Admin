2d3857d6b25ed455cd8943cc8e0b3d8eb7064230
YachaTree <dnftksghkfkd@icloud.com>
2024-11-17 19:42:34 +0900

    Feat: 데이터 받아오기 수정

--- react-admin/src/scenes/domain/apis/memberAPI.jsx
+++ react-admin/src/scenes/domain/apis/memberAPI.jsx
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getMemberList = async () => {
   try {
-    const response = await axios.get('http://localhost:8080//api/auth/members', { withCredentials: true });  // 백엔드 API 호출
-    return response.data;  // 응답 데이터를 반환
+    const response = await axios.get(
+      "http://192.168.0.31:8080/api/auth/members",
+      { withCredentials: true }
+    ); // 백엔드 API 호출
+    return response.data.response; // 응답 데이터를 반환
   } catch (error) {
-    console.error('유저 리스트를 불러오는데 실패했습니다:', error);
+    console.error("유저 리스트를 불러오는데 실패했습니다:", error);
     throw error;
   }
 };
